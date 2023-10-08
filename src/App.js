708165c27121ab4b270f18823bcafd92f5b591c0
Sophie-Ndobe <ndobes76@gmail.com>
2023-10-08 21:58:11 +0200

    Created a Search.js component and added a search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Search from "./Search";
+
 function App() {
   return (
     <div className="App">
-      <h1>The Awesome Weather App</h1>
+      <div className="wrapper">
+  
+        <Search />
+      </div>
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Search.css";
+
+export default function Search() {
+  return (
+    <div className="Search">
+      <form>
+        <input type="search" />
+        <button className="btn btn-primary  mb-1">Search</button>
+        <button className="btn btn-success ms-1 mb-1">Current</button>
+      </form>
+    </div>
+  );
+}
